723376f294a76e93402652001d1748ade8be00cf
Ulukbek Turatbekov <ulukbekturatbekov@axelkg.com>
2025-08-13 18:23:00 +0600

    one_more_commit

--- app/Models/Rubric.php
+++ app/Models/Rubric.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Rubric extends Model
 {
-    //
+    use softDeletes;
+    public function rubrics(): HasMany {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    public function parent(): BelongsTo {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
 }
--- database/migrations/2025_08_13_085224_create_rubrics_table.php
+++ database/migrations/2025_08_13_085224_create_rubrics_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->string('name');
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->foreign('parent_id')->references('id')->on('rubrics')->onDelete('restrict');
+
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
